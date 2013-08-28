9ca5b77ed0e9b8057450c98f1d30c58f48647030
Clinton Cecil <ccecil@ctacorp.com>
2013-08-28 15:38:45 -0500

    added card insertion in helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
+  def insert_cards(full_document)
+    full_document.gsub(/\[card:(.*)\]/) do
+      "#{image_tag("SVG-cards-1.3/"+$1.tr(" ","_")+".svg", height: '64')}"
+    end
+  end
   def markdown(content)
     @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, hard_wrap: true, filter_html: true, no_intraemphasis: true, fenced_code: true, gh_blockcode: true)
-    @markdown.render(content).html_safe
+    @markdown.render(insert_cards(content)).html_safe
   end
 end
