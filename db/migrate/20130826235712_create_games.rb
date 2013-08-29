c008fe3a17232492ef5c3b632d85ad7ac30f0115
Clinton Cecil <ccecil@ctacorp.com>
2013-08-28 22:56:39 -0500

    fixed initial description

--- db/migrate/20130826235712_create_games.rb
+++ db/migrate/20130826235712_create_games.rb
@@ -2,33 +2,7 @@ class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
       t.string :name
-      t.text :description, default: "Description:
-      -
-
-      {description goes here}
-      
-      Related Games:
-      -
-      [game:sample]
-
-      Walkthrough:
-      -
-
-      {sample walkthrough to be replaced
-      Initial deal - 
-
-       * player 1 - [card:ace of spades][card:two of spades]
-       * player 2 - [card:ace of clubs][card:two of clubs]
-       * player 3 - [card:ace of hearts][card:two of hearts]
-
-      Round of betting
-
-      Deal a community card - [card:king of diamonds]
-
-      Round of betting
-
-      Declare winner
-      }"
+      t.text :description, default: "Description:\n-\n\n{description goes here}\nRelated Games:\n-\n[game:sample]\n\nWalkthrough:\n-\n\n{sample walkthrough to be replaced\nInitial deal - \n\n * player 1 - [card:ace of spades][card:two of spades]\n * player 2 - [card:ace of clubs][card:two of clubs]\n * player 3 - [card:ace of hearts][card:two of hearts]\n\nRound of betting\n\nDeal a community card - [card:king of diamonds]\n\nRound of betting\n\nDeclare winner\n}"
       t.belongs_to :style, index: true
       t.integer :min_players
       t.integer :max_players
